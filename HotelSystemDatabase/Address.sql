44059a1a0394a6c2042fbc23c00a26a9c8905331
IzabelaKrawczyk <56648098+IzabelaKrawczyk@users.noreply.github.com>
2020-08-06 15:45:30 +0200

    Normalized database of the hotel system

---
+++ HotelSystemDatabase/Address.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[AddressId] INT NOT NULL PRIMARY KEY, 
+    [StreetName] CHAR(10) NOT NULL, 
+    [StreetNumber] INT NOT NULL, 
+    [FlatNumber] CHAR(10) NULL, 
+    [CityName] TEXT NOT NULL, 
+    [PostalCode] CHAR(10) NOT NULL
+
+)
